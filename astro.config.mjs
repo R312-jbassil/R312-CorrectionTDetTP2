87ff98e74fc11356d98cbd039e91843f443a78aa
jadbassil <31135113+jadbassil@users.noreply.github.com>
2025-09-02 11:05:47 +0200

    Update Astro configuration and dependencies; adjust DaisyUI themes

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 
 
+import node from '@astrojs/node';
+
+
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
